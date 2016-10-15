ae77c96e6e6988cccd90dc130ee1a2456547b0fc
q2git <54710@sohu.com>
2016-10-15 17:17:06 +0800

    Update find_trend.py

--- find_trend.py
+++ find_trend.py
@@ -39,27 +39,30 @@ def db2df(years='2016', ktype='D', table='stocks'):
     return df      
 
 
-def add_Cols(df, *args):
+def add_Cols(df, **kwargs):
     ''' add moving average to dataframe '''
     
     print 'Adding columns to dataframe...',
+    mas = kwargs.setdefault('ma', [30, 60])
+    rds = kwargs.setdefault('rds', [60, 90])
     
     df0 = df.set_index('date').sort_index()
     
-    for day in args:
+    for day in mas:
         ma = df0.groupby('code')['close'].rolling(day).mean()\
              .reset_index().rename(columns={'close':'ma{}'.format(day)})
         
         df = pd.merge(df, ma, on=['code', 'date'])
-  
-    c_max = df0.groupby('code')['close'].rolling(90,min_periods=30).max()\
-            .reset_index().rename(columns={'close':'c_max'})
-            
-    c_min = df0.groupby('code')['close'].rolling(90,min_periods=30).min()\
-            .reset_index().rename(columns={'close':'c_min'})
+        
+    for day in rds:
+        c_max = df0.groupby('code')['close'].rolling(day).max()\
+                .reset_index().rename(columns={'close':'roll_max{}'.format(day)})
+                
+        c_min = df0.groupby('code')['close'].rolling(day).min()\
+                .reset_index().rename(columns={'close':'roll_min{}'.format(day)})
     
-    df = pd.merge(df, c_max, on=['code', 'date'])
-    df = pd.merge(df, c_min, on=['code', 'date'])
+        df = pd.merge(df, c_max, on=['code', 'date'])
+        df = pd.merge(df, c_min, on=['code', 'date'])
 
     print 'Done.'
     
@@ -70,11 +73,11 @@ def fun1(s):
     """ apply function """
     
     kwargs = {}
-    pct_coff = 1 #100/s.code.count() #to percentage
+    pct_coff = 100.0/s.code.count() #to percentage
     # p change
-    for i in [0.1, 7]:
-        k1 = 'pc_A{:.0f}'.format(i)
-        k2 = 'pc_B{:.0f}'.format(i)
+    for i in [1, 7]:
+        k1 = 'pc_pos{:.0f}'.format(i)
+        k2 = 'pc_neg{:.0f}'.format(i)
         kwargs[k1] = np.where(s['p_change']>=i, 1.0, 0).sum() * pct_coff
         kwargs[k2] = np.where(s['p_change']<=-i, 1.0, 0).sum() * pct_coff   
 
@@ -83,29 +86,30 @@ def fun1(s):
     f = lambda x: 'ma{}'.format(x)
 
     for ma in mas_c[::2]:
-        k1 = 'ma_A{:02d}'.format(ma) #above
-        k2 = 'ma_B{:02d}'.format(ma) #below        
-        kwargs[k1] = np.where(s['close']>=s[f(ma)], 1.0, 0).sum() * pct_coff        
-        kwargs[k2] = np.where(s['close']<s[f(ma)], 1.0, 0).sum() * pct_coff   
+        k1 = 'over_ma{}'.format(ma) #above      
+        kwargs[k1] = np.where(s['close']>=s[f(ma)], 1.0, 0).sum() * pct_coff          
     
     for i in [5, 20]:
         idx = mas_c.index(i)
         _mas = zip(mas_c[idx:], mas_c[idx+1:])
-        k1 = 'ma_U{:02d}'.format(i) #up trend
-        k2 = 'ma_D{:02d}'.format(i) #down trend 
+        k1 = 'trend_rise{}'.format(i) #up trend (close>=ma5>=10>=20...)
+        k2 = 'trend_fall{}'.format(i) #down trend (close<ma5<10<20...)
         c1 = reduce(lambda m,n: m&n, map(lambda (x,y):s[f(x)]>=s[f(y)], _mas))
         c2 = reduce(lambda m,n: m&n, map(lambda (x,y):s[f(x)]<s[f(y)], _mas)) 
         kwargs[k1] = np.where(c1, 1.0, 0).sum() * pct_coff
         kwargs[k2] = np.where(c2, 1.0, 0).sum() * pct_coff     
 
     # close, amplitude, volumn
-    kwargs['avg_c'] =  np.multiply(s['close'], s['volume']).sum() / s['volume'].sum()
-    kwargs['avg_a'] = (((s['high']-s['low']) / s['low']).mean()) * 100
-    kwargs['avg_v'] = s['volume'].mean()          
+    kwargs['avg_close'] =  np.multiply(s['close'], s['volume']).sum() / s['volume'].sum()
+    kwargs['avg_swing'] = (((s['high']-s['low']) / s['low']).mean()) * 100
+    kwargs['avg_volume'] = s['volume'].mean()          
  
-    #above expanding_max, below expanding_min
-    kwargs['c_max'] = np.where(s['close']>=s['c_max'], 1.0, 0).sum() 
-    kwargs['c_min'] = np.where(s['close']<=s['c_min'], 1.0, 0).sum() 
+    # expanding_max
+    for i in [60, 90]:
+        k1 = 'roll_max{}'.format(i) #above
+        k2 = 'roll_min{}'.format(i) #below          
+        kwargs[k1] = np.where(s['close']>=s[k1], 1.0, 0).sum() 
+        kwargs[k2] = np.where(s['close']<=s[k2], 1.0, 0).sum() 
     
     ser = pd.Series(data=kwargs.values(), index=kwargs.keys()).sort_index()
     
@@ -117,11 +121,11 @@ def stat(years='2016', w=None):
     
     dfidxs = db2df(years, table='indexs')
     dfidxs = dfidxs.groupby(['code','date'])['close'].max().unstack(0)
-    dfidxs.rename(columns=lambda x:'zs_{}'.format(x), inplace=True)
+    dfidxs.rename(columns=lambda x:'idx_{}'.format(x), inplace=True)
     #dfidx = dfidxs[idx].rename('zs_{}'.format(idx))
     
     dfstks = db2df(years)
-    dfstks = add_Cols(dfstks, 30, 60)
+    dfstks = add_Cols(dfstks)
 
     print 'Performing statistics...',
     
@@ -129,12 +133,14 @@ def stat(years='2016', w=None):
    
     if w:
         for colname in df.columns:
-            if not colname.startswith('ma_'):
+            if colname.startswith(('pc_','over_ma', 'roll')):
                 df[colname] = df[colname].rolling(window=w).mean()
-                df.rename(columns={colname:'{}_{}'.format(colname, w)},inplace=True)
+                df.rename(columns={colname:'{}_w{}'.format(colname, w)},inplace=True)
 
     df = pd.concat([dfidxs, df], axis=1, join_axes=[dfidxs.index]) #.sort_index(1)
-
+    #df.to_excel('stat.xlsx')     
+    df.index = pd.to_datetime(df.index, format="%Y-%m-%d") #.to_period(freq='D')
+    #df.index = pd.to_datetime(df.index).to_period(freq='D')    
     print 'Done.'
     
     return df
@@ -142,21 +148,20 @@ def stat(years='2016', w=None):
     
 def plot1(df, ref='sh'):
     
-    df.index = pd.to_datetime(df.index, format="%Y-%m-%d") #.to_period(freq='D')
-    #df.index = pd.to_datetime(df.index).to_period(freq='D') 
+    print 'Preparing for figures...',
 
     colnames = df.columns
     #ref = next((x for x in colnames if x.startswith('zs_')))
-    ref = 'zs_{}'.format(ref) 
+    ref = 'idx_{}'.format(ref) 
     
-    for key in ['pc_A','pc_B','ma_A','ma_B','ma_U','ma_D','avg','c_m','zs']:
+    for key in ['pc','over_ma','trend','avg','roll','idx']:
         
         ks = filter(lambda x: x.startswith(key), colnames)
-        if key != 'zs': 
+        if key != 'idx': 
             ks.append(ref)
 
         axes = df[ks].plot(kind='line', grid=True, subplots=True, legend=False, 
-                           rot=0, marker='o',)
+                           rot=0, marker='.',)
 
         fig = plt.gcf() 
 
@@ -176,7 +181,8 @@ def plot1(df, ref='sh'):
         fig.set_size_inches(16, 9) 
         fn = '{}_{}'.format(TODAY, key)               
         fig.savefig(os.path.join(STAT_FOLDER, fn), dpi=200) 
-               
+
+    print 'Done.'               
 
         
 def main():
@@ -190,8 +196,10 @@ def main():
     if w: w=int(w)
     
     df = stat(years, w)
-    
-    plot1(df, idx)
+    from plottings import plot_pair, save_fig
+    figs = plot_pair(df)
+    save_fig(**figs)
+    #plot1(df, idx)
  
     if raw_input('Show plot?: '):
         plt.show() 
