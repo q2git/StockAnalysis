760ce31c509dcbf2860291023ae82b8ee0dde14b
q2git <54710@sohu.com>
2016-10-17 17:20:17 +0800

    Update statfuncs.py

--- statfuncs.py
+++ statfuncs.py
@@ -4,19 +4,15 @@ Created on Tue Oct 11 16:53:08 2016
 @author: q2git
 """
 
-import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
 import pandas as pd
 import numpy as np
 import sqlite3
 import os
-from commons import TODAY, DATA_DIR, STAT_DIR
+from commons import DATA_DIR, STAT_DIR
 
     
 def db2df(years='2016', ktype='D', table='stocks'):
-    """ reading data from db and return DataFrame object """
-    
-    df = pd.DataFrame()
+    """ reading data from db and return a generator """
     
     for year in years.split('.'):
  
@@ -24,35 +20,33 @@ def db2df(years='2016', ktype='D', table='stocks'):
     
         with sqlite3.connect(db) as con:
             sql = 'select * from {}'.format(table)  #stocks or indexs 
-            
             print 'Reading table [{}] from [{}] ...'.format(table, db), 
-            df = df.append(pd.read_sql(sql, con), ignore_index=True)
+            df = pd.read_sql(sql, con)
             print 'Done.'
     
-    return df      
+        yield df      
 
 
-def add_Cols(df, **kwargs):
-    ''' add moving average to dataframe '''
+def add_Cols(df, ma_days=[30,60], rmxx_days=[30,60]):
+    """ add columns moving average and rolling-max/min to dataframe 
+    usage: df = add_MAs_RMs(df, mas=[30,60], rms=[30,90]) """
     
     print 'Adding columns to dataframe...',
-    mas = kwargs.setdefault('ma', [30, 60])
-    rds = kwargs.setdefault('rds', [60, 90])
-    
+   
     df0 = df.set_index('date').sort_index()
     
-    for day in mas:
+    for day in ma_days:
         ma = df0.groupby('code')['close'].rolling(day).mean()\
              .reset_index().rename(columns={'close':'ma{}'.format(day)})
         
         df = pd.merge(df, ma, on=['code', 'date'])
         
-    for day in rds:
+    for day in rmxx_days:
         c_max = df0.groupby('code')['close'].rolling(day).max()\
-                .reset_index().rename(columns={'close':'roll_max{}'.format(day)})
+                .reset_index().rename(columns={'close':'rmax{}'.format(day)})
                 
         c_min = df0.groupby('code')['close'].rolling(day).min()\
-                .reset_index().rename(columns={'close':'roll_min{}'.format(day)})
+                .reset_index().rename(columns={'close':'rmin{}'.format(day)})
     
         df = pd.merge(df, c_max, on=['code', 'date'])
         df = pd.merge(df, c_min, on=['code', 'date'])
@@ -62,123 +56,88 @@ def add_Cols(df, **kwargs):
     return df 
     
 
-def fun1(s):
-    """ apply function """
+def stat_daily(s):
+    """ apply function for daily """
     
     kwargs = {}
     pct_coff = 100.0/s.code.count() #to percentage
     # p change
-    for i in [1, 7]:
-        k1 = 'pc_pos{:.0f}'.format(i)
-        k2 = 'pc_neg{:.0f}'.format(i)
+    p_changes=[1,3,5,7,9]
+    for i in p_changes:
+        k1 = 'p_change > +{:.0f}%'.format(i)
+        k2 = 'p_change < -{:.0f}%'.format(i)
         kwargs[k1] = np.where(s['p_change']>=i, 1.0, 0).sum() * pct_coff
         kwargs[k2] = np.where(s['p_change']<=-i, 1.0, 0).sum() * pct_coff   
 
+    mas = s.columns.str.extract('(^ma\d+)').dropna().tolist() #extract ma
+    mas.sort(key=lambda x: int(x[2:])) 
     # ma trends
-    mas_c = [5, 10, 20, 30, 60]
-    f = lambda x: 'ma{}'.format(x)
-
-    for ma in mas_c[::2]:
-        k1 = 'over_ma{}'.format(ma) #above      
-        kwargs[k1] = np.where(s['close']>=s[f(ma)], 1.0, 0).sum() * pct_coff          
+    for ma in mas:
+        k1 = 'close > {}'.format(ma) #above      
+        kwargs[k1] = np.where(s['close']>=s[ma], 1.0, 0).sum() * pct_coff          
     
-    for i in [5, 20]:
-        idx = mas_c.index(i)
-        _mas = zip(mas_c[idx:], mas_c[idx+1:])
-        k1 = 'trend_rise{}'.format(i) #up trend (close>=ma5>=10>=20...)
-        k2 = 'trend_fall{}'.format(i) #down trend (close<ma5<10<20...)
-        c1 = reduce(lambda m,n: m&n, map(lambda (x,y):s[f(x)]>=s[f(y)], _mas))
-        c2 = reduce(lambda m,n: m&n, map(lambda (x,y):s[f(x)]<s[f(y)], _mas)) 
+    trends=[0,1,2] #trends[0] means close>ma5>ma10>ma..
+    for i in trends:
+        _mas = mas[i:]
+        _mas.insert(0,'close')
+        cmp_pairs = zip(_mas[i:], _mas[i+1:])
+        k1 = 'trend_up:' + ' > '.join(_mas) #up trend (close>=ma5>=10>=20...)
+        k2 = 'trend_down:' +' < '.join(_mas) #down trend (close<ma5<10<20...)
+        c1 = reduce(lambda m,n: m&n, map(lambda (x,y):s[x]>=s[y], cmp_pairs))
+        c2 = reduce(lambda m,n: m&n, map(lambda (x,y):s[x]<s[y], cmp_pairs)) 
         kwargs[k1] = np.where(c1, 1.0, 0).sum() * pct_coff
         kwargs[k2] = np.where(c2, 1.0, 0).sum() * pct_coff     
 
-    # close, amplitude, volumn
+    # rolling_max/min
+    rmaxs = s.columns.str.extract('(^rmax\d+)').dropna().tolist() #extract rmax
+    rmins = s.columns.str.extract('(^rmin\d+)').dropna().tolist() #extract rmin
+    for rmax, rmin in zip(rmaxs, rmins):
+        k1 = 'close = {}'.format(rmax) #close = highest
+        k2 = 'close = {}'.format(rmin) #close = lowest          
+        kwargs[k1] = np.where(s['close']==s[rmax], 1.0, 0).sum() 
+        kwargs[k2] = np.where(s['close']==s[rmin], 1.0, 0).sum() 
+
+    # close, swing, volumn
     kwargs['avg_close'] =  np.multiply(s['close'], s['volume']).sum() / s['volume'].sum()
-    kwargs['avg_swing'] = (((s['high']-s['low']) / s['low']).mean()) * 100
+    kwargs['avg_swing'] = ((s['high']-s['low']) / s['low']).mean()
     kwargs['avg_volume'] = s['volume'].mean()          
- 
-    # expanding_max
-    for i in [60, 90]:
-        k1 = 'roll_max{}'.format(i) #above
-        k2 = 'roll_min{}'.format(i) #below          
-        kwargs[k1] = np.where(s['close']>=s[k1], 1.0, 0).sum() 
-        kwargs[k2] = np.where(s['close']<=s[k2], 1.0, 0).sum() 
-    
+     
     ser = pd.Series(data=kwargs.values(), index=kwargs.keys()).sort_index()
     
     return ser                    
                      
                      
-def stat(years='2016', w=None):
+def stat(years='2016',  w=None, add_mas=[30,60], add_rmxxs=[30,90]):
     """ statistic analysis """
     
-    dfidxs = db2df(years, table='indexs')
-    dfidxs = dfidxs.groupby(['code','date'])['close'].max().unstack(0)
-    dfidxs.rename(columns=lambda x:'idx_{}'.format(x), inplace=True)
-    #dfidx = dfidxs[idx].rename('zs_{}'.format(idx))
+    # indexs
+    dfi = pd.concat(db2df(years, table='indexs'), ignore_index=True)
+    dfi = dfi.groupby(['code','date'])['close'].max().unstack(0)
+    dfi.rename(columns=lambda x:'idx_{}'.format(x), inplace=True)   
     
-    dfstks = db2df(years)
-    dfstks = add_Cols(dfstks)
-
+    df = pd.concat(db2df(years), ignore_index=True)
+    df = add_Cols(df, add_mas, add_rmxxs)
+    
     print 'Performing statistics...',
     
-    df = dfstks.groupby('date').apply(fun1)
+    df = df.groupby('date').apply(stat_daily) # statistical analysis
    
     if w:
         for colname in df.columns:
-            if colname.startswith(('pc_','over_ma', 'roll')):
+            if colname.startswith(('p_change',)):
                 df[colname] = df[colname].rolling(window=w).mean()
                 df.rename(columns={colname:'{}_w{}'.format(colname, w)},inplace=True)
 
-    df = pd.concat([dfidxs, df], axis=1, join_axes=[dfidxs.index]) #.sort_index(1)
-    #df.to_excel('stat.xlsx')     
+    df = pd.concat([dfi, df], axis=1, join_axes=[dfi.index]) #.sort_index(1)
+    df.to_excel(os.path.join(STAT_DIR, 'stat_{}_daily.xlsx'.format(years)))     
     df.index = pd.to_datetime(df.index, format="%Y-%m-%d") #.to_period(freq='D')
     #df.index = pd.to_datetime(df.index).to_period(freq='D')    
     print 'Done.'
     
-    return df
-
+    return df       
     
-def plot1(df, ref='sh'):
     
-    print 'Preparing for figures...',
-
-    colnames = df.columns
-    #ref = next((x for x in colnames if x.startswith('zs_')))
-    ref = 'idx_{}'.format(ref) 
-    
-    for key in ['pc','over_ma','trend','avg','roll','idx']:
-        
-        ks = filter(lambda x: x.startswith(key), colnames)
-        if key != 'idx': 
-            ks.append(ref)
-
-        axes = df[ks].plot(kind='line', grid=True, subplots=True, legend=False, 
-                           rot=0, marker='.',)
-
-        fig = plt.gcf() 
-
-        for ax in axes:
-            ax.legend(loc='upper left', prop={'size':8},) #, bbox_to_anchor=(1.0, 0.5))
-            ax.xaxis.set_minor_locator(mdates.WeekdayLocator(interval=1))           
-            ax.xaxis.set_minor_formatter(mdates.DateFormatter('%d'))
-            #ax.xaxis.grid(True, which="minor") 
-            ax.xaxis.set_major_locator(mdates.MonthLocator(interval=1))            
-            ax.xaxis.set_major_formatter(mdates.DateFormatter('%b\n%Y'))  
-            ax.tick_params(axis='both', which='both', labelsize=8)
-
-        plt.tick_params(axis='x', which='minor', labelsize=6, )
-        plt.tick_params(axis='x', which='major', pad=20, )        
-        plt.setp(axes[-1].xaxis.get_minorticklabels(), rotation=90) 
-
-        fig.set_size_inches(16, 9) 
-        fn = '{}_{}'.format(TODAY, key)               
-        fig.savefig(os.path.join(STAT_DIR, fn), dpi=200) 
-
-    print 'Done.'               
-
-        
-def main():
+if __name__ == '__main__':
     years = raw_input('Years(=2016, 2015.2016): ')
     if not years: years='2016'
     
@@ -189,16 +148,3 @@ def main():
     if w: w=int(w)
     
     df = stat(years, w)
-    from plottings import plot_pair, save_fig, plot_line
-    figs1 = plot_pair(df)
-    figs2 = plot_line(df)
-    save_fig(**figs1)
-    save_fig(**figs2)
-    #plot1(df, idx)
- 
-    if raw_input('Show plot?: '):
-        plt.show() 
-    
-    
-if __name__ == '__main__':
-    main()
