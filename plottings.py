61e15476853cbef373b2bc9ac75f82e4e3fbbe2f
q2git <54710@sohu.com>
2016-10-17 17:28:57 +0800

    Update plottings.py

--- plottings.py
+++ plottings.py
@@ -89,5 +89,63 @@ def plot_line(df, ref='sh'):
         fig = plt.gcf()
         figs['{}'.format(prefix)] = fig
     print 'Done.' 
+
+
+def plot1(df, ref='sh'):
     
-    return figs
+    print 'Preparing for figures...',
+
+    colnames = df.columns
+    #ref = next((x for x in colnames if x.startswith('zs_')))
+    ref = 'idx_{}'.format(ref) 
+    
+    for key in ['pc','over_ma','trend','avg','roll','idx']:
+        
+        ks = filter(lambda x: x.startswith(key), colnames)
+        if key != 'idx': 
+            ks.append(ref)
+
+        axes = df[ks].plot(kind='line', grid=True, subplots=True, legend=False, 
+                           rot=0, marker='.',)
+
+        fig = plt.gcf() 
+
+        for ax in axes:
+            ax.legend(loc='upper left', prop={'size':8},) #, bbox_to_anchor=(1.0, 0.5))
+            ax.xaxis.set_minor_locator(mdates.WeekdayLocator(interval=1))           
+            ax.xaxis.set_minor_formatter(mdates.DateFormatter('%d'))
+            #ax.xaxis.grid(True, which="minor") 
+            ax.xaxis.set_major_locator(mdates.MonthLocator(interval=1))            
+            ax.xaxis.set_major_formatter(mdates.DateFormatter('%b\n%Y'))  
+            ax.tick_params(axis='both', which='both', labelsize=8)
+
+        plt.tick_params(axis='x', which='minor', labelsize=6, )
+        plt.tick_params(axis='x', which='major', pad=20, )        
+        plt.setp(axes[-1].xaxis.get_minorticklabels(), rotation=90) 
+
+        fig.set_size_inches(16, 9) 
+
+    print 'Done.'    
+    
+    return fig
+    
+if __name__ == '__main__':
+    years = raw_input('Years(=2016, 2015.2016): ')
+    if not years: years='2016'
+    
+    idx = raw_input('Index(=sh,sz,hs300,sz50,zxb,cyb): ')
+    if not idx: idx='sh'
+    
+    w = raw_input('Window size(=None): ')
+    if w: w=int(w)
+    
+    from statfuncs import stat
+    df = stat(years, w)
+    figs1 = plot_pair(df)
+    figs2 = plot_line(df)
+    save_fig(**figs1)
+    save_fig(**figs2)
+    #plot1(df, idx)
+ 
+    if raw_input('Show plot?: '):
+        plt.show()     
